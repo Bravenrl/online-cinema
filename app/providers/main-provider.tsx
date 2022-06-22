e76f0f86d64e0a2b8e90adc0a96c72f2befa6754
Bravenrl <bravenrl@gmail.com>
2022-06-23 01:00:38 +0300

    add head-provider

--- app/providers/main-provider.tsx
+++ app/providers/main-provider.tsx
@@ -1,7 +1,12 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 
 import Layout from '@/components/layout/layout';
 import StyledToastContainer from '@/components/ui/styled-toast-container/styled-toast-container';
+
+import { store } from '@/store/store';
+
+import HeadProvider from './head-provider/head-provider';
 
 type MainProviderProps = {
   children: React.ReactNode;
@@ -17,10 +22,14 @@ const queryClient = new QueryClient({
 
 function MainProvider({ children }: MainProviderProps): JSX.Element {
   return (
-    <QueryClientProvider client={queryClient}>
-      <StyledToastContainer />
-      <Layout>{children}</Layout>
-    </QueryClientProvider>
+    <HeadProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <StyledToastContainer />
+          <Layout>{children}</Layout>
+        </QueryClientProvider>
+      </Provider>
+    </HeadProvider>
   );
 }
 export default MainProvider;
---
+++ app/store/root-reducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+export const RootReducer = combineReducers({
+
+  });
+  
+  export type TypeRootState = ReturnType<typeof RootReducer>;
+  
